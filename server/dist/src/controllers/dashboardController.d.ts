89bcbb379a06256d28eeb4c85f30d3b70220102c
yao <ycheng11036@gmail.com>
2025-09-07 19:08:07 -0400

    Add client folder

---
+++ server/dist/src/controllers/dashboardController.d.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from "express";
+export declare const getDashboardMetrics: (req: Request, res: Response) => Promise<void>;
+//# sourceMappingURL=dashboardController.d.ts.map
