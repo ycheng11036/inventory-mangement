b9f7f6bc0fa4527d8955eb6ded20ca6bcdff5b3a
yao <ycheng11036@gmail.com>
2025-09-15 19:37:19 -0400

    75% completed project

--- client/src/app/expenses/page.tsx
+++ client/src/app/expenses/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
-import { useGetExpensesByCategoryQuery } from "@/state/api";
+import {
+  ExpenseByCategorySummary,
+  useGetExpensesByCategoryQuery,
+} from "@/state/api";
 import React, { useMemo, useState } from "react";
 import Header from "@/app/(components)/Header";
 import { ClassNames } from "@emotion/react";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+
+type AggregateDataItem = {
+  name: string;
+  color?: string;
+  amount: number;
+};
+
+type AggregatedData = {
+  [category: string]: AggregateDataItem;
+};
 
 const Expenses = () => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -21,6 +42,39 @@ const Expenses = () => {
   } = useGetExpensesByCategoryQuery();
 
   const expenses = useMemo(() => expensesData ?? [], [expensesData]);
+
+  const parseDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toISOString().split("T")[0];
+  };
+
+  const aggregatedData: AggregatedDataItem[] = useMemo(() => {
+    const filterd: AggregatedData = expenses
+      .filter((data: ExpenseByCategorySummary) => {
+        const matchesCategory =
+          selectedCategory === "All" || data.category === selectedCategory;
+        const dataDate = parseDate(data.date);
+        const matchesDate =
+          !startDate ||
+          !endDate ||
+          (dataDate >= startDate && dataDate <= endDate);
+
+        return matchesCategory && matchesDate;
+      })
+      .reduce((acc: AggregatedData, data: ExpenseByCategorySummary) => {
+        const amount = parseInt(data.amount);
+        if (!acc[data.category]) {
+          acc[data.category] = { name: data.category, amount: 0 };
+          acc[data.category].color = `#${Math.floor(
+            Math.random() * 16777215
+          ).toString(16)}`;
+          acc[data.category].amount += amount;
+        }
+        return acc;
+      }, {});
+
+    return Object.values(filterd);
+  }, [expenses, selectedCategory, startDate, endDate]);
 
   const classNames = {
     label: "block text-sm font-medium text-gray-700",
@@ -40,6 +94,7 @@ const Expenses = () => {
 
   return (
     <div>
+      {/* HEADER */}
       <div className="mb-5">
         <Header name="Expenses" />
         <p className="text-sm text-gray-500">
@@ -54,6 +109,7 @@ const Expenses = () => {
             Filter by Category and Date
           </h3>
           <div className="space-y-4">
+            {/* CATEGORY */}
             <div>
               <label htmlFor="category" className={classNames.label}>
                 Category
@@ -71,7 +127,63 @@ const Expenses = () => {
                 <option>Salaries</option>
               </select>
             </div>
+            {/* START DATE  */}
+            <div>
+              <label htmlFor="start-date" className={classNames.label}>
+                Start Date
+              </label>
+              <input
+                id="start-date"
+                type="date"
+                name="start-date"
+                className={classNames.selectInput}
+                onChange={(e) => setStartDate(e.target.value)}
+              />
+            </div>
+            {/* END DATE  */}
+            <div>
+              <label htmlFor="end-date" className={classNames.label}>
+                End Date
+              </label>
+              <input
+                id="end-date"
+                type="date"
+                name="end-date"
+                className={classNames.selectInput}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
           </div>
+        </div>
+        {/* PIE CHART */}
+        <div className="flex-grow bg-white shadow rounded-lg p-4 md:p-6">
+          <ResponsiveContainer width="100%" height={400}>
+            <PieChart>
+              <Pie
+                data={aggregatedData}
+                cx="50%"
+                cy="50%"
+                label
+                outerRadius={150}
+                fill="#8884d8"
+                dataKey="amount"
+                onMouseEnter={(_, index) => setActiveIndex(index)}
+              >
+                {aggregatedData.map(
+                  (entry: AggregateDataItem, index: number) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={
+                        index === activeIndex ? "rgb(29, 78, 216)" : entry.color
+                      }
+                    />
+                  )
+                )}
+              </Pie>
+              <Tooltip />
+              <Legend />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
