05bb3512a7a93c4cb55f22fe99211b39d024cc59
yao <ycheng11036@gmail.com>
2025-09-15 21:10:51 -0400

    add log statement to check the url at build

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
+console.log(">>> NEXT_PUBLIC_API_BASE_URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
